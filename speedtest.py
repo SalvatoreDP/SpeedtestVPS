870cb7ef3583cc0cdb550f58c94f9e7bf81e528f
SalvatoreDP <soldoni@gmail.com>
2014-05-11 16:37:54 +0200

    Create speedtest.py

---
+++ speedtest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2014 Salvatore Della Pepa
+# All Rights Reserved.
+#
+#                              Licensed under 
+#                   GNU GENERAL PUBLIC LICENSE version 2
+#
+#     you may not use this file except in compliance with the License. 
+#               You may obtain a copy of the License at
+#
+#                http://www.gnu.org/licenses/gpl-2.0.html
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+__version__ = '0.1.2'
